dd51ae5db737a3ee46782a245366beb7c98ab8c3
gnapiorkowski <gustaw.napiorkowski@gmail.com>
2020-08-05 19:19:56 +0200

    first commit

--- #10/deep learning/tmp.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('dogsVScats.csv')
-df.loc[(df['label'] >= 0.5) & df['id'].str.contains('cat'), 'true'] = 0
-df.loc[(df['label'] < 0.5) & df['id'].str.contains('cat'), 'true'] = 1
-df.loc[(df['label'] < 0.5) & df['id'].str.contains('dog'), 'true'] = 1
-df.loc[(df['label'] >= 0.5) & df['id'].str.contains('dog'), 'true'] = 0
-print('Accuracy: ', 1. - len(df.loc[df['true'] == 0].index)/len(df.index))
-print(df.loc[df['true'] == 0])
